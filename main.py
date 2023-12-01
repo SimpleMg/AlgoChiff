cf15dc1a3e6f4fae441d5b8dc9cf125cd11b61b0
Litex986 <135990772+Litex986@users.noreply.github.com>
2023-12-01 10:52:58 +0100

    Update main.py
    init

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+class Encrypt:
+    def __init__(self, KEY, message):
+        self.key = Key(KEY)
+        self.message = self.splitMessage(message)
+        self.key.keyBase = self.key.deriveKeys(self.key.KEY, 4)
+        self.key.keys[0] = self.key.deriveKeys(self.key.keyBase[0], 5)
+        self.key.keys[1] = self.key.deriveKeys(self.key.keyBase[1], 16)
+        self.key.keys[2] = self.key.deriveKeys(self.key.keyBase[2], 5)
+        self.key.keys[3] = self.key.deriveKeys(self.key.keyBase[3], 2)
+
+
+    def splitMessage(self, bits) -> list:
+        pass
