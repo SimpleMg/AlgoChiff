8beb41a9ec49852b2c4182734349f67a776d3193
Litex986 <135990772+Litex986@users.noreply.github.com>
2023-12-01 18:25:06 +0100

    Update Encrypt + decrypt

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class Encrypt:
         self.layer(0)
     
     def splitMessage(self, bits) -> list:
-        return ['\x14c.3!2{#g3ml\x7fD25aBm+7x2x.3lng.O=bkKg&e);%pl142qIlg8Ccr>zgzpe90g{', '\x122{</k&!n2h=-\x16N\x7f(md\x12>(8{;u#0n;2y\x12Eys=`Dhz8\x7f<,!7:=i#\x1bE|$9?Fawh%5z', '\x1fe,1%9zw`58>zX::+a\x7f|A:qhx`$\x7f24:2+\x02ce&9+{\x11g/0,6&!4nh3uW<3&k"r\x1deu0', '\x12`}g"=rwo7jk)Qld\x15f(c\x7fmw~lj<b"\x00fhC5*l&8y(2m1m"^h<H8%ltbu-felf!\x0e1>']
-        ['Je m"appelle Minh', 'Je m"appelle MArtin', 'Je m"appelle Vecteur', 'Je m"appelle Zeo']
+
+        return [self.func.intToHex(self.func.stringToInt('Je m"appelle Minh')), self.func.intToHex(self.func.stringToInt('Je m"appelle MArtin')), self.func.intToHex(self.func.stringToInt('Je m"appelle Vecteur')), self.func.intToHex(self.func.stringToInt('Je m"appelle Zeo'))]
 
     def initialisationKeys(self):
         self.key.keyBase = self.key.deriveKeys(self.key.KEY, 4)
@@ -41,12 +41,46 @@ class Encrypt:
                 key = self.key.deriveKeys(self.key.keys[layer][j], 3)
                 key[0] = self.adaptationKey(self.message[i], key[0])
                 self.message[i] = self.func.xor(key[0], self.message[i])
-                #self.message[i] = self.func.intToHex(self.func.stringToInt(self.message[i]))
                 #self.message[i] = self.func.funcKey[j%4][0](self.message[i] , key[1])
-                key[2] = self.adaptationKey(self.message[i], key[2])
+                key[1] = self.adaptationKey(self.message[i], key[2])
                 self.message[i] = self.func.xor(key[2], self.message[i])
-        print(self.message)
+        
   
+    
+    def adaptationKey(self, message, key):
+        if len(key) == len(message):
+            return key
+        else:
+            return self.key.deriveKeys(key, 1, len(message))[0]
+            
+
+
+
+class Decrypt:
+    def __init__(self, KEY, message):
+        self.key = Key(KEY)
+        self.func = allFunc()
+        self.message = message
+        self.initialisationKeys()
+        self.layer(0)
+    
+    def initialisationKeys(self):
+        self.key.keyBase = self.key.deriveKeys(self.key.KEY, 4)
+        self.key.keys[0] = self.key.deriveKeys(self.key.keyBase[0], 5)
+        self.key.keys[1] = self.key.deriveKeys(self.key.keyBase[1], 16)
+        self.key.keys[2] = self.key.deriveKeys(self.key.keyBase[2], 5)
+        self.key.keys[3] = self.key.deriveKeys(self.key.keyBase[3], 2)
+    
+    def layer(self, layer):
+        for i in range(1, len(self.message) + 1):
+            for j in range(1, 6):
+                key = self.key.deriveKeys(self.key.keys[layer][-j], 3)
+                key[2] = self.adaptationKey(self.message[-i], key[2])
+                self.message[-i] = self.func.xor(key[2], self.message[-i])
+                #self.message[-i] = self.func.funcKey[-(j%4)][0](self.message[-i] , key[1])
+                key[0] = self.adaptationKey(self.message[-i], key[0])
+                self.message[-i] = self.func.xor(key[0], self.message[-i])
+        
     
     def adaptationKey(self, message, key):
         if len(key) == len(message):
