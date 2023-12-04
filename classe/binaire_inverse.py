18b5b6a8d10c4350400de12c912253572df4bf55
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-12-04 11:14:31 +0100

    Update binaire_inverse.py

--- classe/binaire_inverse.py
+++ classe/binaire_inverse.py
@@ -1,5 +1,4 @@
 def inverse_binaire(nombre_binaire):
-	nombre_inverse = ''.join('0' if bit == '1' else '1' for bit in nombre_binaire)
-	return nombre_inverse
+	return ''.join('0' if bit == '1' else '1' for bit in nombre_binaire)
 
 print(inverse_binaire("01010101100"))
